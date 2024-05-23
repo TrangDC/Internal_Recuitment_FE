edfe6368049a492b78df25ddfc1ab1064724e007
Andrew Pham (TECHVIYF.ITS) <andrew.pham@techvify.com.vn>
2024-05-23 09:39:13 +0000

    [FIX] Build

--- src/features/calendars/presentation/page-sections/google-calendar/BackdropLoading.tsx
+++ src/features/calendars/presentation/page-sections/google-calendar/BackdropLoading.tsx
@@ -1,4 +1,5 @@
 import { Backdrop, CircularProgress } from '@mui/material'
+import { Fragment } from 'react/jsx-runtime'
 import { Text15sb } from 'shared/components/Typography'
 import FlexBox from 'shared/components/flexbox/FlexBox'
 
@@ -8,28 +9,30 @@ type BackdropLoadingProps = {
 
 function BackdropLoading({ isLoading }: BackdropLoadingProps) {
   return (
-    isLoading && (
-      <Backdrop
-        sx={{
-          color: '#fff',
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          backgroundColor: 'rgba(0, 0, 0, 0.2)',
-        }}
-        open={isLoading}
-      >
-        <FlexBox flexDirection={'column'} alignItems={'center'} gap={2}>
-          <CircularProgress color="inherit" />
-          <Text15sb color={'grey.900'}>
-            One moment. Loading your calendar...
-          </Text15sb>
-        </FlexBox>
-      </Backdrop>
-    )
+    <Fragment>
+      {isLoading && (
+        <Backdrop
+          sx={{
+            color: '#fff',
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          }}
+          open={isLoading}
+        >
+          <FlexBox flexDirection={'column'} alignItems={'center'} gap={2}>
+            <CircularProgress color="inherit" />
+            <Text15sb color={'grey.900'}>
+              One moment. Loading your calendar...
+            </Text15sb>
+          </FlexBox>
+        </Backdrop>
+      )}
+    </Fragment>
   )
 }
 export default BackdropLoading
