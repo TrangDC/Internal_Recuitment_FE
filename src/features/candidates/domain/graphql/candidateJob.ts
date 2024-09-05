1d37fd6ef2a17d70735b2ec8488e247c37470bf8
Andrew Pham (TECHVIYF.ITS) <andrew.pham@techvify.com.vn>
2024-09-05 09:38:26 +0000

    [FIX] Bugs

--- src/features/candidates/domain/graphql/candidateJob.ts
+++ src/features/candidates/domain/graphql/candidateJob.ts
@@ -24,6 +24,7 @@ const useCandidateJobGraphql = () => {
             id
             name
             location
+            status
             hiring_team {
                id
                name
